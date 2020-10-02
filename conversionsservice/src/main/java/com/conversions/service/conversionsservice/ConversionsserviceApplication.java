d9539568e9e0abd3bdb0b4ce14b09c71e523a6e1
User-PC <User-PC@192.168.1.118>
2020-10-02 18:57:17 +0200

    Created Project after fixing Github password

---
+++ conversionsservice/src/main/java/com/conversions/service/conversionsservice/ConversionsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.conversions.service.conversionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionsserviceApplication.class, args);
+	}
+
+}
---
+++ conversionsservice/src/test/java/com/conversions/service/conversionsservice/ConversionsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversions.service.conversionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
